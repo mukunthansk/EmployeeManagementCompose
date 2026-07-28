d39d8acf841ebb9356cfa8fa13f4a6e0126763c3
mukunthansk <mukunthansk8@gmail.com>
2026-07-28 12:01:30 +0530

    Add Spring Boot main application class

---
+++ src/main/java/com/example/EmployeeManagementComposeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementComposeApplication.class, args);
+    }
+
+}
